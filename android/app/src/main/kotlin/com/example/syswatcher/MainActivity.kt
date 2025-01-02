fa7a2d77b6c065234a83449bce69d261b0bf34ca
HugeKC <20868603+HugeKC01@users.noreply.github.com>
2025-01-02 20:43:32 +0700

    Blank Apps

---
+++ android/app/src/main/kotlin/com/example/syswatcher/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.syswatcher
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/about.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'This is a Flutter Material3 app.',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
